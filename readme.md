27974ed8d0a994684eda64c805955554ae676800
Alex Kempton <alex@funwithtriangles.net>
2019-04-15 10:11:28 +0100

    Basic webpack setup

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Simultaneous Cameras Demo
+Proof of concept to show that simultaneous camera streams are possible for one device on the web.
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      meta: {
+        'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1',
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: '0.0.0.0',
+  },
+  watch: true,
+  devtool: 'cheap-eval-source-map',
+}
